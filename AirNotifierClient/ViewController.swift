27dfc5d7c7f9c59d45869990294e28a27dd8c628
Dongsheng Cai <d@tux.im>
2015-11-06 18:24:24 +1100

    Initial Commit

---
+++ AirNotifierClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirNotifierClient
+//
+//  Created by Dongsheng Cai on 6/11/2015.
+//  Copyright © 2015 Dongsheng Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
